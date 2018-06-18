416758bb145be389084d8b768843e8d356e0fc04
Andrii Dmytrenko <andrii.dmytrenko@deliveroo.co.uk>
2018-06-18 09:19:31 +0100

    Add rb_num2long. Change signature of rb_num2int

--- src/fixnum.rs
+++ src/fixnum.rs
@@ -1,6 +1,7 @@
-use types::{c_long, SignedValue, Value};
+use types::{c_int, c_long, SignedValue, Value};
 
 extern "C" {
     pub fn rb_int2inum(num: SignedValue) -> Value;
-    pub fn rb_num2int(num: Value) -> c_long;
+    pub fn rb_num2int(num: Value) -> c_int;
+    pub fn rb_num2long(num: Value) -> c_long;
 }
